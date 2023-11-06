98352b8b9ce3453951dac1fe1efa45fe09f4f1e5
mohit579-pixel <114941107+mohit579-pixel@users.noreply.github.com>
2023-11-06 08:33:39 +0530

    Update dfs.py

--- dfs.py
+++ dfs.py
@@ -1,37 +1,21 @@
-class Graph:
-    def __init__(self):
-        self.graph = {}
+graph = {
+    '7': [],
+    '5': ['3', '7'],
+    '3': ['2', '4'],
+    '4': ['8'],
+    '2': [],
+    '8': []
+}
 
-    def add_edge(self, u, v):
-        if u in self.graph:
-            self.graph[u].append(v)
-        else:
-            self.graph[u] = [v]
-        if v in self.graph:
-            self.graph[v].append(u)
-        else:
-            self.graph[v] = [u]
+visited = set()
 
-    def dfs_recursive(self, node, visited):
+def dfs(visited, graph, node):
+    if node not in visited:
         visited.add(node)
-        print(node, end=" ")
+        print(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
 
-        for neighbor in self.graph.get(node, []):
-            if neighbor not in visited:
-                self.dfs_recursive(neighbor, visited)
-
-    def dfs(self, start):
-        visited = set()
-        self.dfs_recursive(start, visited)
-
-# Example usage:
-g = Graph()
-g.add_edge(0, 1)
-g.add_edge(0, 2)
-g.add_edge(1, 2)
-g.add_edge(2, 0)
-g.add_edge(2, 3)
-g.add_edge(3, 3)
-
-print("DFS starting from vertex 2:")
-g.dfs(2)
+# Driver Code
+print("The Depth-First Search for the graph is:")
+dfs(visited, graph, '5')
